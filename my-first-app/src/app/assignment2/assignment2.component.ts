7f210bb719090fce1e016c3725b40065b089ebec
Christian Jay D.C <christianjaydelacruz0626@gmail.com>
2021-08-17 19:46:33 +0800

    Assingment 2 Combining all lectures

---
+++ my-first-app/src/app/assignment2/assignment2.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component implements OnInit {
+
+  username = '';
+  allowReset = false;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onReset() {
+    this.username = '';
+  }
+
+}
