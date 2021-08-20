0273fc067b03bee9464132617c6dae3932a30070
Christian Jay D.C <christianjaydelacruz0626@gmail.com>
2021-08-20 17:34:54 +0800

    Initial set up for the project

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipiesComponent } from './recipies/recipies.component';
 import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipies/recipe-lists/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
