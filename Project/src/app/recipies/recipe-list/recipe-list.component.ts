3ef0c2268fa734408e6489f61863cdbd853d58b1
Christian Jay D.C <christianjaydelacruz0626@gmail.com>
2021-08-20 20:09:25 +0800

    Working with recipe list

--- Project/src/app/recipies/recipe-list/recipe-list.component.ts
+++ Project/src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Simpleng Testing lang', 'https://revistanossa.com.br/midias/artigos/Imagens/Filipino_Pork_Adobo-1568724825.jpg'),
+    new Recipe('A Test Recipe', 'Simpleng Testing lang', 'https://4.bp.blogspot.com/-LHPeDpZzzqU/VJ0tP1_8BPI/AAAAAAAACmo/4F5a1W6EO38/s1600/107805-filipino-pork-adobo.png')
+  ];
 
   constructor() { }
 
