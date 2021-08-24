48ec9d1aa580ec2ea1fbd1eee083303e52900eb7
Christian Jay D.C <christianjaydelacruz0626@gmail.com>
2021-08-24 15:25:12 +0800

    Worked on the Assignment4(Odd and Even)

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { FormsModule } from '@angular/forms';
 import { Assignment2Component } from './assignment2/assignment2.component';
 import { Assignment3Component } from './assignment3/assignment3.component';
+import { Assignment4Component } from './assignment4/assignment4.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { Assignment3Component } from './assignment3/assignment3.component';
     WarningAlertComponent,
     SuccessAlertComponent,
     Assignment2Component,
-    Assignment3Component
+    Assignment3Component,
+    Assignment4Component
   ],
   imports: [
     BrowserModule,
