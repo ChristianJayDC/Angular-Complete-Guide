d48ef971df137667b8c0841c104eb6a58b1058fe
Christian Jay D.C <christianjaydelacruz0626@gmail.com>
2021-08-27 21:21:17 +0800

    Finished routing

--- routing/src/app/error-page/error-page.component.ts
+++ routing/src/app/error-page/error-page.component.ts
@@ -7,15 +7,17 @@ import { ActivatedRoute, Data } from '@angular/router';
   styleUrls: ['./error-page.component.css']
 })
 export class ErrorPageComponent implements OnInit {
-
   errorMessage: string;
 
   constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.route.data.subscribe((data: Data) => {
-      this.errorMessage = data['message'];
-    })
+  ngOnInit() {
+    // this.errorMessage = this.route.snapshot.data['message'];
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.errorMessage = data['message'];
+      }
+    );
   }
 
 }
--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -8,8 +8,7 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit, OnDestroy {
-  user: { id: number, name: string };
-
+  user: {id: number, name: string};
   paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
@@ -19,11 +18,13 @@ export class UserComponent implements OnInit, OnDestroy {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-
-    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
-      this.user.id = params['id'];
-      this.user.name = params['name'];
-    });
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
   ngOnDestroy() {
